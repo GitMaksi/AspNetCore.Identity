bb43bad0df79f03b39d1724cdcb1c045c8a28bcb
Maks Sudoł <maksssudol@gmail.com>
2025-01-03 09:26:21 +0100

    Working Identity

--- Program.cs
+++ Program.cs
@@ -2,39 +2,59 @@ using AspNetCore.Identity.Database;
 using AspNetCore.Identity.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Security.Claims;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
-
-builder.Services.AddIdentityCore<User>()
-    .AddEntityFrameworkStores<ApplicationDBContext>()
-    .AddApiEndpoints();
-
-builder.Services.AddDbContext<ApplicationDBContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-    app.ApplyMigrations();
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddAuthorization();
+
+        builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+            .AddCookie(IdentityConstants.ApplicationScheme)
+            .AddBearerToken(IdentityConstants.BearerScheme);
+
+        builder.Services.AddIdentityCore<User>()
+            .AddEntityFrameworkStores<ApplicationDBContext>()
+            .AddApiEndpoints();
+
+        builder.Services.AddDbContext<ApplicationDBContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.ApplyMigrations();
+        }
+
+        app.MapGet("users/me", async (ClaimsPrincipal claims, ApplicationDBContext context) =>
+        {
+            string userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
+            return await context.Users.FindAsync(userId);
+        })
+        .RequireAuthorization();
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.MapIdentityApi<User>();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-app.MapIdentityApi<User>();
-
-app.Run();
